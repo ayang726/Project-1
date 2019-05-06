db0d688485d0375240c9c8824bb27e8d6061dcd2
PaulinaBootcamp <paulinaoshafi@gmail.com>
2019-05-05 21:09:56 -0400

    new buttons in the footer

---
+++ js/generateFooter.js
@@ -0,0 +1,40 @@
+var generateFooter = {
+
+    text: {
+        1: 'Best Practices',
+        2: 'Donate Now',
+
+    },
+
+    additionalClassIfAny: {
+        1: 'bestPractices',
+        2: 'donate',
+
+    },
+
+    icon: {
+        1: 'school',
+        2: 'monetization_on',
+
+    },
+
+    link: {
+        1: '',
+        2: 'href=https://act.autismspeaks.org/site/Donation2?idb=135287805&4141.donation=form1&DONATION_LEVEL_ID_SELECTED=1&df_id=4141&mfc_pref=T&idb=[[S76:idb]]"',
+
+    },
+
+
+}
+
+
+for (var i = 0; i < Object.keys(generateFooter.text).length; i++) {
+    $('#footer').append(`
+    <a target="_blank"
+    ${Object.values(generateFooter.link)[i]}
+    button class="btn waves-effect deep-orange ${Object.values(generateFooter.additionalClassIfAny)[i]}" type="submit" name="action">${Object.values(generateFooter.text)[i]}
+    <i class="material-icons left">${Object.values(generateFooter.icon)[i]}</i>
+    </button></a>  
+  `)
+
+};
