b503fd571190151d8734427b008934862e7b39ff
PaulinaBootcamp <paulinaoshafi@gmail.com>
2019-05-06 12:36:18 -0400

    created js for navbar to be applied to all pages

---
+++ js/generateNavBar.js
@@ -0,0 +1,25 @@
+$("#navBar").append(`
+<nav class="deep-orange lighten-2" role="navigation">
+            <!-- site title -->
+            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">NowYou!</a>
+                <!-- link to login page -->
+
+
+                <div class="row right" class="right hide-on-med-and-down">
+                    <a href="./SigninAuth.html" class="btn-large waves-effect waves-light deep-orange"
+                        id="login-btn">User
+
+                        Login</a>
+                </div>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="./index.html"">Exercises</a></li>
+                    <li><a href="./faqs.html">FAQ</a></li>
+                    <li><a href="#">The Team</a></li>
+                </ul>
+
+            </div>
+        </nav>
+
+
+
+`)
