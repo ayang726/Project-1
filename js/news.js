586b52a2e083b910482dc555c690d1764fa66d94
Maria <lrabin@gmail.com>
2019-05-07 14:41:52 -0400

    Made changes on news api parameters

--- js/news.js
+++ js/news.js
@@ -1,18 +1,24 @@
 
 
 function newsDisplay() {
-    news()
-    console.log(newRow);
-}
+    news();
+
+};
 
 // Function to run when the button is clicked
 function news() {
     var api_key = "b7864e77cbca46e68dae6627855def48";
     var q = "autism";
     var newRow = "";
+    var date = "201-01-01";
+    var sort = "publishedAt";
+    //var category = "health";
+    var lang = "en";
+    var page = "20";
 
 
-    var queryURL = "https://newsapi.org/v2/everything?q=kids%autism&from=2019-04-30&sortBy=publishedAt&apiKey=b7864e77cbca46e68dae6627855def48&limit=3";
+
+    var queryURL = "https://newsapi.org/v2/everything?q=" + q + "&from" + date + "&sortBy=" + sort + "&apiKey=" + api_key + "&language=" + lang + "&pagesize=" + page;
 
     $.ajax({
         url: queryURL,
@@ -20,20 +26,20 @@ function news() {
         dataType: "json"
     }).then(function (response) {
         var displaySourceTitle = response.articles[2].title;
-        var displaySourceName = response.articles[0].source.name;
-        var displaySourceDesc = response.articles[3].description;
+        //var displaySourceName = response.articles[0].source.name;
+        //var displaySourceDesc = response.articles[3].description;
         var displaySourceSite = response.articles[4].url;
 
         var newRow = $("<tr>").append(
             $("<td>").html(displaySourceTitle),
-            $("<td>").html(displaySourceDesc),
-            $("<td>").html(displaySourceName),
+            //$("<td>").html(displaySourceDesc),
+            //$("<td>").html(displaySourceName),
             $("<td>").html(displaySourceSite)
         );
-        console.log(newRow);
+
 
         //$("#article-table > tbody").append(newRow);
 
-        $("#article").html("Source: " + displaySourceName + " Title: " + displaySourceTitle + "Description: " + displaySourceDesc + "Website: " + displaySourceSite);
+        $("#article").html(newRow);
     });
 };
