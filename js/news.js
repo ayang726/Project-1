085d790c3be0f56325f0030addb7020aceed7193
Maria <lrabin@gmail.com>
2019-05-07 15:54:53 -0400

    Updated the faq to include the news article & removed the news.html file

--- js/news.js
+++ js/news.js
@@ -1,22 +1,18 @@
 
 window.onload = newsDisplay();
+
 function newsDisplay() {
     news();
-
 };
 
-// Function to run when the button is clicked
+// Function to run when the window is loaded
 function news() {
     var api_key = "b7864e77cbca46e68dae6627855def48";
     var q = "autism";
-    var newRow = "";
-    var date = "201-01-01";
-    var sort = "publishedAt";
-    //var category = "health";
+    var date = "2019-01-01";
+    var sort = "relevancy";
     var lang = "en";
     var page = "20";
-
-
 
     var queryURL = "https://newsapi.org/v2/everything?q=" + q + "&from" + date + "&sortBy=" + sort + "&apiKey=" + api_key + "&language=" + lang + "&pagesize=" + page;
 
@@ -28,19 +24,16 @@ function news() {
         for (var i = 0; i < response.articles.length; i++) {
             console.log(response.articles.length);
             var displaySourceTitle = response.articles[i].title;
-            var displaySourceSite = response.articles[i].url;
+            var displaySourceSite = response.articles[i].url
+            var displayContent = response.articles[i].content
+            //var displaySourceSiteImg = response.articles[i].urlToImage;
 
-            var newRow = $("<tr>").append(
-                $("<tr>").html(displaySourceTitle),
-                $("<tr>").html(displaySourceSite)
+            var newRow = $("<li>").append(
+                $("<li>").html("Title: " + displaySourceTitle),
+                $("<li>").html("Source: " + displaySourceSite),
+                $("<li>").html("Content: " + displayContent)
             );
-
             $("#article").append(newRow).append('&nbsp;');
         };
-
-        // $(document).ready(function () {
-        //});
-
-
     });
 };
