384c25765d728d75be92808950b70b01e2aad134
Lindsey Duquette <duquette.lr@gmail.com>
2019-05-08 19:44:55 -0400

    html for pt dash and useless js pt dash page

---
+++ js/PTprofile.js
@@ -0,0 +1,11 @@
+console.log("ALERT ALERT ALERT")
+console.log(`this is what I want` + firebase.auth());
+let storageRef = storage.ref('/video').child
+
+function addStudent(studentProfile) {
+
+    $(".studentProfileLinks").append(`<li class="collection-item">${studentProfile}</li>`)
+
+};
+
+addStudent(storageRef);
