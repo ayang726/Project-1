2dae165b4881251c60bdb8d976fb9df721bdf8cf
AwesomePresent <yoursmile128@hotmail.com>
2019-05-08 23:41:41 -0400

    added news collapsible, other bug and minor fixes

--- js/news.js
+++ js/news.js
@@ -1,19 +1,19 @@
-
-window.onload = newsDisplay();
-
-function newsDisplay() {
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
     news();
-};
+});
+
+
 
 // Function to run when the window is loaded
 function news() {
     var api_key = "b7864e77cbca46e68dae6627855def48";
-    var q = "autism";
-    var date = "2019-01-01";
+    var q = "autism spectrum";
+    var date = "1990-01-01";
     var sort = "relevancy";
     var lang = "en";
-    var page = "20";
-
+    var page = "8";
     var queryURL = "https://newsapi.org/v2/everything?q=" + q + "&from" + date + "&sortBy=" + sort + "&apiKey=" + api_key + "&language=" + lang + "&pagesize=" + page;
 
     $.ajax({
@@ -22,18 +22,20 @@ function news() {
         dataType: "json"
     }).then(function (response) {
         for (var i = 0; i < response.articles.length; i++) {
-            console.log(response.articles.length);
+            // console.log(response.articles.length);
             var displaySourceTitle = response.articles[i].title;
             var displaySourceSite = response.articles[i].url
             var displayContent = response.articles[i].content
             //var displaySourceSiteImg = response.articles[i].urlToImage;
 
-            var newRow = $("<li>").append(
-                $("<li>").html("Title: " + displaySourceTitle),
-                $("<li>").html("Source: " + displaySourceSite),
-                $("<li>").html("Content: " + displayContent)
-            );
-            $("#article").append(newRow).append('&nbsp;');
+            var html = `
+            <li>
+            <div class="collapsible-header"><i class="material-icons">filter_drama</i>${displaySourceTitle}</div>
+            <div class="collapsible-body"><a href="${displaySourceSite}">${displayContent}</a></div>
+            </li>
+        `
+            $(".collapsible").append(html);
         };
+
     });
 };
